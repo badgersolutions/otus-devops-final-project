8159754eb30b89c134906f228660f1f86ba8c842
Denis Barsukov <DBarsukov@alfabank.ru>
2020-04-19 22:36:56 +0300

    fix build path in docker-compose.dev.yml, add scripts

---
+++ services/publish-docker-images-to-github-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# https://github.com/settings/tokens
+
+export USER_LOGIN=bard86
+
+cat ~/GH_TOKEN | docker login docker.pkg.github.com -u $USER_LOGIN --password-stdin
+
+export WORKDIR=`pwd`
+
+for service in account-service auth-service config gateway monitoring notification-service registry statistics-service turbine-stream-service
+do
+  cd $WORKDIR/../src/$service
+  docker build -t docker.pkg.github.com/badgersolutions/otus-devops-final-project/piggymetrics_$service:latest .
+  docker push docker.pkg.github.com/badgersolutions/otus-devops-final-project/piggymetrics_$service:latest
+done
