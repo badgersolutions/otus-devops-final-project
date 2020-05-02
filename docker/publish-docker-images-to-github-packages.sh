97b05ade5ea65bc2f776dfebc1a8f22b158b0d6c
Denis Barsukov <DBarsukov@alfabank.ru>
2020-05-02 09:27:47 +0300

    update mcs configs (uri for k8s)

--- docker/publish-docker-images-to-github-packages.sh
+++ docker/publish-docker-images-to-github-packages.sh
@@ -3,11 +3,11 @@ set -e
 
 # https://github.com/settings/tokens
 
-export USER_LOGIN=bard86
+USER_LOGIN=bard86
 
 cat ~/GH_TOKEN | docker login docker.pkg.github.com -u $USER_LOGIN --password-stdin
 
-export WORKDIR=`pwd`
+WORKDIR=$(pwd)
 
 for service in account-service auth-service config gateway monitoring notification-service registry statistics-service turbine-stream-service
 do
@@ -16,6 +16,6 @@ do
   docker push docker.pkg.github.com/badgersolutions/otus-devops-final-project/piggymetrics_$service:latest
 done
 
-cd $WORKDIR/../service/mongodb
+cd $WORKDIR/../services/mongodb
 docker build -t docker.pkg.github.com/badgersolutions/otus-devops-final-project/piggymetrics_mongodb:latest .
 docker push docker.pkg.github.com/badgersolutions/otus-devops-final-project/piggymetrics_mongodb:latest
